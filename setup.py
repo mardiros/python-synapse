485d6539f35a3fb2d8dd0c91e1751f201c39244a
mardiros <mardiros@laposte.net>
2012-12-01 14:29:28 +0100

    Fix the pep8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
-#CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+# CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 with open(os.path.join(here, 'synapse', '__init__.py')) as v_file:
     version = re.compile(r".*__version__ = '(.*?)'",
@@ -20,13 +20,13 @@ requires = ['pyzmq >= 2.1.1',
 setup(name='synapse',
       version=version,
       description='Distributed communication module',
-      long_description=README, # + '\n\n' +  CHANGES,
+      long_description=README,  # + '\n\n' +  CHANGES,
       author='Greg Leclercq',
       author_email='greg@0x80.net',
       url='http://github.org/ggreg/python-synapse',
       packages=find_packages(),
       install_requires=requires,
-      test_requires=requires + ['redis'],
+      test_requires=requires,
       zip_safe=False,
       test_suite='synapse'
-     )
+      )
