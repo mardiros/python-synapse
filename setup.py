4a585f22a7ea1d22e8ac3c71d3cf05dbff98005e
mardiros <mardiros@laposte.net>
2012-12-01 14:22:10 +0100

    Fix setuptool and set version number in the package

--- setup.py
+++ setup.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python
 import os
 
-#from distutils.core import setup
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 #CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
+with open(os.path.join(here, 'synapse', '__init__.py')) as v_file:
+    version = re.compile(r".*__version__ = '(.*?)'",
+                         re.S).match(v_file.read()).group(1)
+
+requires = ['pyzmq >= 2.1.1',
+            'gevent_zeromq == 0.2.0',
+            'redis',
+            'gevent == 0.13.6',
+            'simplejson']
 
 setup(name='synapse',
-      version       = '0.3',
-      description   = 'Distributed communication module',
+      version=version,
+      description='Distributed communication module',
       long_description=README, # + '\n\n' +  CHANGES,
-      author        = 'Greg Leclercq',
-      author_email  = 'greg@0x80.net',
-      url           = 'http://github.org/ggreg/python-synapse',
-      packages      = ['synapse'],
-      requires      = ['pyzmq (>= 2.1.1)', 'gevent_zeromq','simplejson'],
+      author='Greg Leclercq',
+      author_email='greg@0x80.net',
+      url='http://github.org/ggreg/python-synapse',
+      packages=find_packages(),
+      install_requires=requires,
+      test_requires=requires + ['redis'],
       zip_safe=False,
       test_suite='synapse'
      )
